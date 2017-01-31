b41808334bb9b7650a443d7c9186e0af8c0fb544
Mateusz <mateusz.szymajda@gmail.com>
2017-01-31 17:45:14 +0100

    Add files via upload

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,25 +1,32 @@
-// check off the specific todos by clicking
-$("ul").on("click", "li", function(){
-	$(this).toggleClass("completed");
+//check off the specific todos by clicking
+$("ul").on("click", "span", function(){
+	$(this).parent().toggleClass("completed");
 });
 
-// click on X to delete todos
+//change class of icon check to redo
+$("ul").on("click", "span.check", function(){
+	$(this).toggleClass("redo");
+	$(this).find("i").toggleClass("fa-check fa-repeat");
+});
 
-$("ul").on("click", "span", function(event){
+//click on trash icon to delete item
+$("ul").on("click", "span.delete", function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation(); 
 });
 
+//click enter to add new todo
 $("input[type=text]").keypress(function(event) {
 	if(event.which === 13) {
 		var todoText = $(this).val();
 		$(this).val("");
-		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+		$("ul").append("<li><span class='delete'><i class='fa fa-trash'></i></span><span class='check'><i class='fa fa-check'></i></span> " + todoText + "</li>")
 	}
 });
 
+//click pencil icon to show input form
 $(".fa-pencil").click(function() {
 	$("input[type=text]").fadeToggle();
 });
